c007beb3bc84395a9029ce6666003252dd2fc51a
Harris Thompson <harris@harrisjt.com>
2019-03-25 20:23:53 -0500

    fix: initial expo impl

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {Provider} from 'react-redux';
+import createStore from './src/store';
+import Home from './src/components/Home';
+
+// Store Initialization
+const initialState = window.__INITIAL_STATE__ || {
+  firebase: {authError: null},
+};
+
+const store = createStore(initialState);
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Provider store={store}>
+          <Home />
+        </Provider>
       </View>
     );
   }
--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,12 +1,15 @@
-export default firebaseConfig = {
-  "type": "service_account",
-  "project_id": "cupsave-e9b32",
-  "private_key_id": "449e877eb4e75522cca4ba09b45c490062bdb010",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDmzgffiEMCQ4ob\n7zhf9qdWQTYpjUefYO3gxnLXSAUXx5abw2hguoPYRHYv68TqeL6vpe21NlMn33zo\n3J0mTtjamEG/KEUrGFcDKPYSC5/F8uQCs5LQV/NeTfgY/D32VGLlKBYdFybmdghM\n+n/u6VRGs43GL00dpHASxLsyy5OFR5hHK/+A5eSNyd+VeiWVtF3GrAN2C1lWqiCo\n+loltfs+gq1BEL5naWueRe+vIVLv6FKoXJvB4iOyc6m0BoZZJi9t0Etgd60+45PN\nXPghTuX/amp40fsmxxaBL1XAE2DaqwxQ13Ivu1KaEjpHV0p4Skxr2PirVGyTwrno\nOwna3qapAgMBAAECggEAN86wk6mofjcjgV6l9Ho4+v+4n3J0U4LF8fg6E3jsFlRh\nw9cLeXwP3i6rnym0ivdYMDOAVzVEBRuiYdHaQ8WD5NM07opHs0Rm4bI5UaKy5tO0\nszyFbtAdXfufVjdDqji8UumO80jCVK5MkoRtg7Cyeof262lLjbPRj5A3LXCRAoQY\nLlbZJvkMGeiwVlF9MrUeWlFb1dAIPRdygZzbJAEVJgjOLbBz6danEmSOuBIfAVF/\nMnz+fSekWKv6UEUyfg/XkVrWbesXs0kcvrw/9Pyl9NXwcTfvXD488zGhY7cI5vD3\nHrwb23EnBS+JQNaXbTpFWhpoJfLZOBJblEtz0GM1qQKBgQDzvq66MZRkfVHshXgu\nboxOSjNxLv3vITE0TDN73vPDhZmwQlQdQSrbIVnrfQeJxdPaETupb4Oh/i3anzJF\nUH0EpiU50Uqtzf/KMjZ76zUKU88m1bRTcXJqwt2aYb3pwuyLtjSi9S4c6HEvoUwU\nt2maVCJ6GFRr34Y/ixCuAqdtDQKBgQDyaMrzvwbhEVHw3YaxzfOcwzn11HN5IlNg\npSEQx3Y6720E//yaARIZaKlXy7JkhRCX9qydaKL+5ngVXiHXminSxcFh0joIcPcs\nSo+u8TblyJs8SLsNR6k5E5FddDeNZzZxroLQJAHVTLdN3QvDVrIkz6xxJMhcvYQb\nVGrCRRFRDQKBgDRgnmVucAfkEi20RK4dnVLZiRJiWbkANfWaBlow/8rdM7h3mGGs\n9kuecBc1h+AoPa0XlO2FBBFUcIARQ5k3gZX1Kfyxp82Pfc7yWVjpoz0z40jiFVz0\n/PRKKZqaNFg+RdyD6+20qBMYM/JbQ+ETxMKgH8SC4+lGYC/Zb10lPtslAoGBAMY+\n14Ph/SFBxe6jbM2q7gmNS7B1D/7ICMIoKdOMHG/0cQc+xvm32HJ5Osz3ZKx6emid\nLuwIsAygtSJcenbVuFCMtCto/1b589sAG2PqF4hyuCnkHT2uFoH8M8znB6uX9lmg\na5EHcYxckiiWl1EobMZYJOHfQBFAWHL1Zd39GD1dAoGARXtSlkSb2Xia9Chm2wJZ\naz7L/1dgeYcyNDUx5wOrR+ZXM0SNm7HOGIyCEjc55g1hFRJpE1OryZsS+3vNJA/O\nahcm4B8sUXoQFg7QSumFVnix/ziAV9QjP3Wwq9IMDQQgDiYgTOGr9OwFKEhO9zt1\n7DSy0rhuNJQsMurUyNo8fCQ=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-0x7uv@cupsave-e9b32.iam.gserviceaccount.com",
-  "client_id": "100926796174674756936",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-0x7uv%40cupsave-e9b32.iam.gserviceaccount.com"
-}
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCKHQbALlbnESV0U3hSA8kQXLFyBLvMFFk',
+  authDomain: 'cupsave-e9b32.firebaseapp.com',
+  databaseURL: 'https://cupsave-e9b32.firebaseio.com',
+  projectId: 'cupsave-e9b32',
+  storageBucket: 'cupsave-e9b32.appspot.com',
+  messagingSenderId: '1058030936457',
+};
+
+export const reactReduxFirebaseConfig = {
+  userProfile: 'users',
+  enableLogging: false,
+};
+
+export default {firebaseConfig, reactReduxFirebaseConfig};
