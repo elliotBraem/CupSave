efa569c4abee969c8f9d8d69555baeb564fa41ca
elliotBraem <braemelliot@gmail.com>
2019-03-29 14:47:32 -0500

    style: replaces react native inputs with nachos

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,32 +1,29 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet, View, TextInput, Alert, Button} from 'react-native';
+import {Text, StyleSheet, View, TextInput, Alert} from 'react-native';
 import PropTypes from 'prop-types';
 import {withFirebase} from 'react-redux-firebase';
-// import {Button} from 'nachos-ui';
+import {Button, Input, H2, H4} from 'nachos-ui';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
-    paddingTop: 15,
-    paddingLeft: 15,
-    paddingRight: 15,
-    justifyContent: 'center',
     flex: 1,
-    alignItems: 'center',
-  },
-  buttons: {
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
-    marginBottom: 10,
+    alignItems: 'center',
   },
+  // buttons: {
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   marginBottom: 10,
+  // },
   header: {
     fontSize: 38,
     marginBottom: 20,
   },
-  button: {
-    marginBottom: 15,
-    justifyContent: 'center',
-  },
+  // button: {
+  //   marginBottom: 15,
+  //   justifyContent: 'center',
+  // },
   textInput: {
     height: 40,
     width: '100%',
@@ -35,6 +32,9 @@ const styles = StyleSheet.create({
     marginTop: 14,
     alignSelf: 'stretch',
     textAlign: 'center',
+  },
+  inputStyle: {
+    margin: 15,
   },
 });
 
@@ -79,26 +79,26 @@ class LoginScreen extends Component {
 
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Welcome to CupSave!</Text>
-        <Text>Let&#39;s get started</Text>
-        <Text>Login</Text>
+        <H2 style={styles.header}>Welcome to CupSave!</H2>
+        <H4>Let&#39;s get started</H4>
+        <H4>Login</H4>
         {this.state.errorMessage && <Text style={{color: 'red'}}>{this.state.errorMessage}</Text>}
-        <TextInput
-          style={styles.textInput}
+        <Input
+          style={styles.inputStyle}
           autoCapitalize="none"
           placeholder="Email"
-          onChangeText={email => this.setState({email})}
           value={this.state.email}
+          onChangeText={email => this.setState({email})}
         />
-        <TextInput
+        <Input
           secureTextEntry
-          style={styles.textInput}
+          style={styles.inputStyle}
           autoCapitalize="none"
           placeholder="Password"
           onChangeText={password => this.setState({password})}
           value={this.state.password}
         />
-        <View style={styles.buttons}>
+        {/* <View style={styles.buttons}>
           <Button title="Login" onPress={this.handleLogin} style={styles.button} />
           <Button
             title="Don't have an account? Sign Up"
@@ -107,7 +107,7 @@ class LoginScreen extends Component {
           />
           <Button title="Open drawer" onPress={() => navigation.openDrawer()} style={styles.button} />
           <Button title="Go back" onPress={() => navigation.navigate('Home')} style={styles.button} />
-        </View>
+        </View> */}
       </View>
     );
   }
