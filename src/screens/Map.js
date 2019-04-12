73b6ca3c16f76b775ac6c733d6140c87def3fbbf
Zach Kremer <zkremer@wisc.edu>
2019-04-12 01:02:23 -0500

    feat: Map screen added

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Text, StyleSheet, View} from 'react-native';
-import {Button} from 'nachos-ui';
+import {Text, StyleSheet, View, Button} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+// import {Button} from 'nachos-ui';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,11 +19,50 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    margin: 15,
-    justifyContent: 'center',
+  btnStyle: {
+    width: '100%',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+  },
+  btnContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    // alignContent: 'space-between',
+  },
+  mapContainer: {
+    width: '100%',
+    height: 200,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
+
+const UserMap = props => {
+  let userLocationMarker = null;
+
+  if (props.userLocation) {
+    userLocationMarker = <MapView.Marker coordinate={props.userLocation} />;
+  }
+
+  return (
+    <View style={styles.mapContainer}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+        region={props.userLocation}>
+        {userLocationMarker}
+      </MapView>
+    </View>
+  );
+};
 
 class MapScreen extends Component {
   static navigationOptions = {
@@ -36,17 +76,39 @@ class MapScreen extends Component {
     }).isRequired,
   };
 
+  state = {userLocation: null};
+
+  componentWillMount() {
+    this.getUserLocation();
+  }
+
+  getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          userLocation: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.0622,
+            longitudeDelta: 0.0421,
+          },
+        });
+      },
+      err => console.log(err)
+    );
+    console.log(this.state.userLocation);
+    console.log(this.state.userLocation);
+  };
+
   render() {
     const {navigation} = this.props;
-
+    const {userLocation} = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Map</Text>
-         <View style={styles.buttons}>
-          <Button onPress={() => navigation.openDrawer()} style={styles.button}>
-            Menu
-          </Button>
-        </View>
+        <Button title="Menu" onPress={() => navigation.openDrawer()} style={styles.btnStyle} />
+        <Button title="Get Location" onPress={() => this.getUserLocation()} style={styles.btnStyle} />
+        <UserMap userLocation={userLocation} />
       </View>
     );
   }
