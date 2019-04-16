5536c84674aae8ce3276d3a3c7f3d129e16de2f2
Harris Thompson <harris@harrisjt.com>
2019-04-16 03:18:05 -0500

    fix: build without metro-config, set history consumption

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,13 +1,14 @@
 import React, {PureComponent} from 'react';
 import {StyleSheet, View, Image, Button} from 'react-native';
 import PropTypes from 'prop-types';
-import {withFirebase} from 'react-redux-firebase';
+import {withFirebase, isLoaded} from 'react-redux-firebase';
 import {withNavigation} from 'react-navigation';
 import {compose} from 'redux';
 import {connect} from 'react-redux';
 import COLORS from '../constants/colors';
 import CustomHeader from '../components/CustomHeader';
 import StatsOverview from '../components/StatsOverview';
+import Loading from '../components/Loading';
 
 const profileImage = require('../assets/images/profileicon.png');
 
@@ -42,23 +43,18 @@ class ProfileScreen extends PureComponent {
     }).isRequired,
   };
 
-  componentDidUpdate(prevProps) {
-    console.log('test');
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log('test2');
-  }
-
   render() {
     const {navigation, profile} = this.props;
+
+    if (!isLoaded(profile)) {
+      return <Loading />;
+    }
 
     return (
       <View style={styles.container}>
         <CustomHeader title="Profile" />
         <Image source={profileImage} style={styles.circle} />
         <StatsOverview
-          key={profile.consumption.total}
           totalCupsSaved={profile.consumption.total}
           level={profile.level}
           drinkSize={profile.cup_volume_oz}
