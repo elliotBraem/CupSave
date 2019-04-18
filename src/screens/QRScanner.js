c647c240b3b1b5b900e448cbc9e908605ca724fa
elliotBraem <braemelliot@gmail.com>
2019-04-18 13:44:17 -0500

    feat: adds qr scanner

--- src/screens/QRScanner.js
+++ src/screens/QRScanner.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, View} from 'react-native';
-import {Button} from 'nachos-ui';
-import {Permissions} from 'expo';
+import {StyleSheet, View, Button, Text} from 'react-native';
+import {BarCodeScanner, Permissions} from 'expo';
 import CustomHeader from '../components/CustomHeader';
 import COLORS from '../constants/colors';
 
@@ -12,13 +11,12 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     backgroundColor: COLORS.primary,
   },
+  qrScanner: {
+    flex: 1,
+  },
 });
 
 class QRScannerScreen extends Component {
-  // static navigationOptions = {
-  //   title: 'QRScanner',
-  // };
-
   static propTypes = {
     navigation: PropTypes.shape({
       openDrawer: PropTypes.func.isRequired,
@@ -28,30 +26,28 @@ class QRScannerScreen extends Component {
 
   state = {hasCameraPermission: null};
 
-  componentDidMount() {
-    this.scan();
-  }
-
-  async scan() {
+  async componentDidMount() {
     const {status} = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted',
-    });
+    this.setState({hasCameraPermission: status === 'granted'});
   }
 
-  constructor(props) {
-    super(props);
-  }
+  handleBarCodeScanned = ({type, data}) => {
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
 
   render() {
+    const {hasCameraPermission} = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
     return (
       <View style={styles.container}>
         <CustomHeader title="QR Scanner" />
-        <View style={styles.buttons}>
-          <Button onPress={this.scan.bind(this)} style={styles.button}>
-            Scan
-          </Button>
-        </View>
+        <BarCodeScanner onBarCodeScanned={this.handleBarCodeScanned} style={styles.qrScanner} />
       </View>
     );
   }
