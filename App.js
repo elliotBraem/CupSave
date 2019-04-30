bfa496a6fde3b85451e9e2dde984513de58aff26
toru5 <zkremer@wisc.edu>
2019-04-29 23:02:28 -0500

    feat: bar charts using chart kit

--- App.js
+++ App.js
@@ -2,5 +2,5 @@ import {AppRegistry} from 'react-native';
 import App from './src';
 
 export default App;
-
+console.ignoredYellowBox = ['Warning:'];
 AppRegistry.registerComponent('CupSave', () => App, false);
--- src/components/WasteOverview.js
+++ src/components/WasteOverview.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {View, StyleSheet, Dimensions} from 'react-native';
 import PropTypes from 'prop-types';
-import {BarChart} from 'react-native-chart-kit';
+import {BarChart, StackedBarChart} from 'react-native-chart-kit';
 import COLORS from '../constants/colors';
 import WasteItem from './WasteItem';
 
 const styles = StyleSheet.create({
   container: {
     width: '95%',
-    height: 90,
+    height: 400,
     backgroundColor: COLORS.white,
     borderRadius: 12,
     flexDirection: 'row',
@@ -38,7 +38,7 @@ const WasteOverview = ({waste, polyPlastic, C02e}) => (
         borderRadius: 16,
       }}
       data={{
-        labels: ['Waste Saved', 'Polyurethane Plastic Saved', 'C02e Saved'],
+        labels: ['Waste\nSaved', 'Polyurethane Plastic Saved', 'C02e Saved'],
         datasets: [
           {
             data: [`${waste}`, `${polyPlastic}`, `${C02e}`],
@@ -46,14 +46,15 @@ const WasteOverview = ({waste, polyPlastic, C02e}) => (
         ],
       }}
       width={Dimensions.get('window').width}
-      height={220}
-      yAxisLabel={'lbs.'}
+      height={400}
+      yAxisLabel={''}
       chartConfig={{
-        backgroundColor: '#e26a00',
-        backgroundGradientFrom: '#fb8c00',
-        backgroundGradientTo: '#ffa726',
-        decimalPlaces: 2, // optional, defaults to 2dp
-        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        backgroundColor: COLORS.primary,
+        backgroundGradientFrom: COLORS.primary,
+        backgroundGradientTo: '#FFBF46',
+        decimalPlaces: 3, // optional, defaults to 2dp
+
+        color: (opacity = 1) => `rgba(87, 87, 97, ${opacity})`,
         style: {
           borderRadius: 16,
         },
