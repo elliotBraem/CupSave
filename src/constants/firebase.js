f5b85681642ec8009285bf9060bc9c6c0c70e431
Harris Thompson <harris@harrisjt.com>
2019-04-30 01:12:38 -0500

    refactor: use react-native-env for env files

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID} from 'react-native-dotenv';
 
 // Firebase creds got from console.firebase.google.com
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
 };
 
 export default firebaseConfig;
