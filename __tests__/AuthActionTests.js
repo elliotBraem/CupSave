f73ac7d9ba05dd63ac8d1eaf7f9e520d5c9c4754
Matthew Strimaitis <matthewstrimaitis@MacBook-Pro.local>
2019-04-30 14:11:21 -0500

    test auth-tests

--- __tests__/AuthActionTests.js
+++ __tests__/AuthActionTests.js
@@ -1,6 +1,10 @@
 import * as ACTIONS from '../src/store/actions/auth';
+import * as ACTIONS2 from '../src/store/actions/badges';
+import * as ACTIONS3 from '../src/store/actions/locations';
+import * as ACTIONS4 from '../src/store/actions/users';
 import TYPES from '../src/constants/auth';
 
+// login
 describe('actions', () => {
   it('should create an action to auth login', () => {
     const user = {};
@@ -11,3 +15,71 @@ describe('actions', () => {
     expect(ACTIONS.login(user)).toEqual(expectedAction);
   });
 });
+
+// updateProfile
+describe('actions', () => {
+  it('should allow user to update profile information', () => {
+    const user = {};
+    const expectedAction = {
+      payload: user,
+      type: TYPES.AUTH_DETAILS_UPDATE,
+    };
+    expect(ACTIONS.updateProfile(user)).toEqual(expectedAction);
+  });
+});
+
+// resetPassword
+describe('actions', () => {
+  it('should reset password for account', () => {
+    const expectedAction = {
+      type: TYPES.AUTH_RESET,
+    };
+    expect(ACTIONS.resetPassword()).toEqual(expectedAction);
+  });
+});
+
+// logout
+describe('actions', () => {
+  it('should logout of account', () => {
+    const expectedAction = {
+      type: TYPES.AUTH_RESET,
+    };
+    expect(ACTIONS.logout()).toEqual(expectedAction);
+  });
+});
+
+// getUserData
+describe('actions', () => {
+  it('should fetch the users data', () => {
+    const user = {};
+    const expectedAction = {
+      payload: user,
+      type: TYPES.AUTH_DETAILS_SET,
+    };
+    expect(ACTIONS.getUserData(user)).toEqual(expectedAction);
+  });
+});
+
+// authLoading
+describe('actions', () => {
+  it('should create an action to auth loading', () => {
+    const expectedAction = {
+      type: TYPES.AUTH_LOADING,
+    };
+    expect(ACTIONS.authLoading()).toEqual(expectedAction);
+  });
+});
+
+// authError
+describe('actions', () => {
+  it('should create an action to auth error', () => {
+    const message = "";
+    const expectedAction = {
+      payload: message,
+      type: TYPES.AUTH_ERROR,
+    };
+    expect(ACTIONS.authError(message)).toEqual(expectedAction);
+  });
+});
+
+
