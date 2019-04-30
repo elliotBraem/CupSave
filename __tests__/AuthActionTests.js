3c9af99807cd6cc277a57229726c72d57b6b5b0b
Matthew Strimaitis <matthewstrimaitis@MacBook-Pro.local>
2019-04-30 16:20:40 -0500

    tests-all-actions

--- __tests__/AuthActionTests.js
+++ __tests__/AuthActionTests.js
@@ -3,6 +3,9 @@ import * as ACTIONS2 from '../src/store/actions/badges';
 import * as ACTIONS3 from '../src/store/actions/locations';
 import * as ACTIONS4 from '../src/store/actions/users';
 import TYPES from '../src/constants/auth';
+import TYPES2 from '../src/constants/badges';
+import TYPES3 from '../src/constants/locations';
+import TYPES4 from '../src/constants/users';
 
 // login
 describe('actions', () => {
@@ -73,7 +76,7 @@ describe('actions', () => {
 // authError
 describe('actions', () => {
   it('should create an action to auth error', () => {
-    const message = "";
+    const message = '';
     const expectedAction = {
       payload: message,
       type: TYPES.AUTH_ERROR,
@@ -82,4 +85,106 @@ describe('actions', () => {
   });
 });
 
+// getBadges
+describe('actions', () => {
+  it('', () => {
+    const badges = {};
+    const expectedAction = {
+      payload: badges,
+      type: TYPES2.BADGES_UPDATE,
+    };
+    expect(ACTIONS2.getBadges(badges)).toEqual(expectedAction);
+  });
+});
+
+// badgesLoading
+describe('actions', () => {
+  it('', () => {
+    const expectedAction = {
+      type: TYPES2.BADGES_LOADING,
+    };
+    expect(ACTIONS2.badgesLoading()).toEqual(expectedAction);
+  });
+});
+
+// badgesError
+describe('actions', () => {
+  it('', () => {
+    const message = "";
+    const expectedAction = {
+      payload: message,
+      type: TYPES2.BADGES_ERROR,
+    };
+    expect(ACTIONS2.badgesError(message)).toEqual(expectedAction);
+  });
+});
+
+// getLocations
+describe('actions', () => {
+  it('', () => {
+    const locations = {};
+    const expectedAction = {
+      payload: locations,
+      type: TYPES3.LOCATIONS_UPDATE,
+    };
+    expect(ACTIONS3.getLocations(locations)).toEqual(expectedAction);
+  });
+});
+
+// locationsLoading
+describe('actions', () => {
+  it('', () => {
+    const expectedAction = {
+      type: TYPES3.LOCATIONS_LOADING,
+    };
+    expect(ACTIONS3.locationsLoading()).toEqual(expectedAction);
+  });
+});
+
+// locationsError
+describe('actions', () => {
+  it('', () => {
+    const message = "";
+    const expectedAction = {
+      payload: message,
+      type: TYPES3.LOCATIONS_ERROR,
+    };
+    expect(ACTIONS3.locationsError(message)).toEqual(expectedAction);
+  });
+});
+
+// getUser (FAILS)
+describe('actions', () => {
+  it('', () => {
+    const email = {};
+    const user = {};
+    const expectedAction = {
+      payload: {email, user},
+      type: TYPES4.USERS_UPDATE,
+    };
+    expect(ACTIONS4.getUser({email, user})).toEqual(expectedAction);
+  });
+});
+
+// userLoading
+describe('actions', () => {
+  it('', () => {
+    const expectedAction = {
+      type: TYPES4.USERS_LOADING,
+    };
+    expect(ACTIONS4.userLoading()).toEqual(expectedAction);
+  });
+});
+
+// userError
+describe('actions', () => {
+  it('', () => {
+    const message = "";
+    const expectedAction = {
+      payload: message,
+      type: TYPES4.USERS_ERROR,
+    };
+    expect(ACTIONS4.userError(message)).toEqual(expectedAction);
+  });
+});
 
